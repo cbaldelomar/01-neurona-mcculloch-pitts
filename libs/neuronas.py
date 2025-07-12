77e6c304e7ed45fea81ab42500deb7572d99e2c1
balde <cbaldelomar@hotmail.com>
2025-07-11 21:13:21 -0600

    Neurona McCulloch-Pitts para datos

--- libs/neuronas.py
+++ libs/neuronas.py
@@ -1,3 +1,12 @@
 class NeuronaMcCullochPittsTodoEnUno:
-    def __init__(self):
+    def __init__(self, input1, input2, input3):
         self.threshold = 1
+        self.input1 = input1
+        self.input2 = input2
+        self.input3 = input3
+
+    def activacion(self):
+        if self.input1 + self.input2 + self.input3 >= self.threshold:
+            return 1
+        else:
+            return 0
--- notebooks/main.py
+++ notebooks/main.py
@@ -5,9 +5,16 @@ from neuronas import NeuronaMcCullochPittsTodoEnUno
 
 
 def main():
-    print("prueba de estar dentro de main")
-    neurona = NeuronaMcCullochPittsTodoEnUno()
-    print(neurona.threshold)
+    tiempo_libre = 0
+    motivacion = 0
+    suscripcion = 0
+    neurona = NeuronaMcCullochPittsTodoEnUno(tiempo_libre, motivacion, suscripcion)
+    decision = neurona.activacion()
+
+    if decision == 1:
+        print("Sí, ir al gym")
+    if decision == 0:
+        print("No, no ir al gym")
 
 
 if __name__ == "__main__":
