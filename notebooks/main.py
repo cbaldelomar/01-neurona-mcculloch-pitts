91be7ba32b79e4ab4d637aae002f0bc12a624b03
balde <cbaldelomar@hotmail.com>
2025-07-11 22:13:39 -0600

    Ejecutar ejemplos D.1.4.

--- notebooks/main.py
+++ notebooks/main.py
@@ -1,17 +1,25 @@
 import sys
 
-sys.path.append("../libs")
+sys.path.append("./libs")
 from neuronas import NeuronaMcCullochPittsTodoEnUno
 
 
 def main():
-    # D.1.1.
+    # D.1.4.
+
+    # tiempo libre (1), motivacion (0), suscripcion (0)
     neurona = NeuronaMcCullochPittsTodoEnUno(2, 1, 0, 0)
     imprimir(neurona.activacion())
 
+    # tiempo libre (1), motivacion (1), suscripcion (0)
+    neurona = NeuronaMcCullochPittsTodoEnUno(2, 1, 1, 0)
+    imprimir(neurona.activacion())
+
+    # tiempo libre (0), motivacion (1), suscripcion (0)
     neurona = NeuronaMcCullochPittsTodoEnUno(2, 0, 1, 0)
     imprimir(neurona.activacion())
 
+    # tiempo libre (0), motivacion (0), suscripcion (1)
     neurona = NeuronaMcCullochPittsTodoEnUno(2, 0, 0, 1)
     imprimir(neurona.activacion())
 
